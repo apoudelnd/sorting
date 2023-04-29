3dfed7505a1a6a7eda80a43d1faa675c199f955f
Amrit <apoudelnd@gmail.com>
2023-04-29 16:12:24 -0400

    insertion sort with rand

---
+++ insert_rand.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    srand(time(0)); // initialize random seed
+
+    int n = 10000; // size of array
+    int arr[n]; // declare array of size n
+
+    // generate random array of integers
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 10000;
+    }
+
+    // sort the array using insertion sort
+    insertionSort(arr, n);
+
+    // print the sorted array
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
