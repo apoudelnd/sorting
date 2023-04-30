0cfc4fdcc333827d48a37b96f4c90c6e20aa1d80
Amrit Poudel <ace@amrits-mbp.dhcp.nd.edu>
2023-04-30 15:44:00 -0400

    new simple and two_level

--- simple.py
+++ simple.py
@@ -1,81 +1,97 @@
-# import the m5 library
-import m5
-
-# import all the simobjects that was compiled into the namespace in this python class
-from m5.objects import *
-
-# instantiate a system
-system = System()
-
-""" set defaults
+# Copyright (c) 2015 Jason Power
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+""" This file creates a barebones system and executes 'hello', a simple Hello
+World application.
+See Part 1, Chapter 2: Creating a simple configuration script in the
+learning_gem5 book for more information about this script.
+IMPORTANT: If you modify this file, it's likely that the Learning gem5 book
+           also needs to be updated. For now, email Jason <power.jg@gmail.com>
+This script uses the X86 ISA. `simple-arm.py` and `simple-riscv.py` may be
+referenced as examples of scripts which utilize the ARM and RISC-V ISAs
+respectively.
 """
-
-## set the clock
-# instantiate a clock domain
+# import the m5 (gem5) library created when gem5 is built
+import m5
+# import all of the SimObjects
+from m5.objects import *
+# create the system we are going to simulate
+system = System()
+# Set the clock frequency of the system (and all of its children)
 system.clk_domain = SrcClockDomain()
-
-# set the clock domain
-system.clk_domain.clock = '1GHz'
-# set the default voltage domain, we are good with the default of the instantiation
+system.clk_domain.clock = "1GHz"
 system.clk_domain.voltage_domain = VoltageDomain()
-
-# set the systems memory mode. different memory mode: timing, atomic, etc. Mostly use timing. Atomic is mostly for
-# fast forwarding and warming caches. Does not actually give timing information. To get timing information, you need
-# timing mode
-system.mem_mode = 'timing'
-
-# set physical memory range
-system.mem_ranges = [AddrRange('512MB')]
-
-# create a cpu: simple simple timing cpu: simple cpu that has single cycle cpu, every thing EXECUTES in one CPU cycle
-# except memory operation, which depends on memory system
-system.cpu = TimingSimpleCPU()
-
-# create the mem bus as shown in the picture
+# Set up the system
+system.mem_mode = "timing"  # Use timing accesses
+system.mem_ranges = [AddrRange("512MB")]  # Create an address range
+# Create a simple CPU
+# You can use ISA-specific CPU models for different workloads:
+# `RiscvTimingSimpleCPU`, `ArmTimingSimpleCPU`.
+system.cpu = X86TimingSimpleCPU()
+# Create a memory bus, a system crossbar, in this case
 system.membus = SystemXBar()
-
-# hook the cpu to the memory bus: cpu and membus are memory objects, so they have a port interface between them, so we
-# set up the ports the CPU has to connect up to the memory bus port
-system.cpu.icache_port = system.membus.slave
-system.cpu.dcache_port = system.membus.slave
-
-# X86 specific stuffs:
-# create a interrupt controller
+# Hook the CPU ports up to the membus
+system.cpu.icache_port = system.membus.cpu_side_ports
+system.cpu.dcache_port = system.membus.cpu_side_ports
+# create the interrupt controller for the CPU and connect to the membus
 system.cpu.createInterruptController()
-# set the X86 cpu so the interrupt works
-system.cpu.interrupts[0].pio = system.membus.master
-system.cpu.interrupts[0].int_master = system.membus.slave
-system.cpu.interrupts[0].int_slave = system.membus.master
-
-system.system_port = system.membus.slave
-
-# create the memory controller, as shown in the diagram
-system.mem_ctrl = DDR3_1600_x64()
-# set this mem_ctrl to our systems range
-system.mem_ctrl.range = system.mem_ranges[0]
-# hook this mem_ctrl to the memory bus
-system.mem_ctrl.port = system.membus.master
-
-# give this system we just created a binary to run 
-process = LiveProcess()
-# give the process a command, inbuilt compiled: in this case is a hello world binary
-process.cmd = ['tests/test-progs/hello/bin/x86/linux/hello']
-# set the cpus workload to be this process
+# For X86 only we make sure the interrupts care connect to memory.
+# Note: these are directly connected to the memory bus and are not cached.
+# For other ISA you should remove the following three lines.
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+# Create a DDR3 memory controller and connect it to the membus
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+# Connect the system up to the membus
+system.system_port = system.membus.cpu_side_ports
+# Here we set the X86 "hello world" binary. With other ISAs you must specify
+# workloads compiled to those ISAs. Other "hello world" binaries for other ISAs
+# can be found in "tests/test-progs/hello".
+thispath = os.path.dirname(os.path.realpath(__file__))
+binary = os.path.join(
+    thispath,
+    "../../../",
+    "tests/test-progs/hello/bin/x86/linux/hello",
+)
+system.workload = SEWorkload.init_compatible(binary)
+# Create a process for a simple "Hello World" application
+process = Process()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
+process.cmd = [binary]
+# Set the cpu to use the process as its workload and create thread contexts
 system.cpu.workload = process
-# create our cpu threads
 system.cpu.createThreads()
-
-# create a root object and pass it our system: all gem5 simluations MUST have a root object
-root = Root(full_system=False)
-root.system = system
-
-# takes all this python objects that we have instantiated and walkthrough all this python object and actually perform
-# the c++ constructors, NOW THE C++ objects ARE CONSTRUCTED
+# set up the root SimObject and start the simulation
+root = Root(full_system=False, system=system)
+# instantiate all of the objects we've created above
 m5.instantiate()
-
-print "Beginning simulation"
+print("Beginning simulation!")
 exit_event = m5.simulate()
-
-# m5.curTick is the tick that the simulation is on while it exits
-print "Done simulating at tick %d because %s" % (m5.curTick(),
-            exit_event.getCause())
+print("Exiting @ tick %i because %s" % (m5.curTick(), exit_event.getCause()))
